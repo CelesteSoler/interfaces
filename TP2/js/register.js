93a0d438d2b5f2b22f9204ce77329c3ec10ee4fd
Melisa Burlando <melisa.burlando@gmail.com>
2023-10-08 05:35:31 -0300

    Agregué una animación al botón de crear cuenta en el registro

--- TP2/js/register.js
+++ TP2/js/register.js
@@ -6,7 +6,14 @@ formulario.addEventListener('submit', registrarUsuario);
 
 function registrarUsuario(event){
     event.preventDefault();
+    let titulo = document.querySelector('#contenedor-titulo');
+    let yaTienes = document.querySelector('#ya-tienes');
+    formulario.style.display="none";
+    titulo.style.display="none";
+    yaTienes.style.display="none";
+    let spinner = document.querySelector('#spinner');
+    spinner.style.display="inline-block";
     let mensajeUsuarioCreado = document.querySelector('#js-mensaje-usuario');
-    mensajeUsuarioCreado.innerHTML = "Su usuario ha sido creado con éxito";
+    mensajeUsuarioCreado.innerHTML = "Su usuario ha sido creado con éxito! Serás redirigido al home";
     setTimeout((()=>window.location.href="home-loged.html"), 5000)
 }
